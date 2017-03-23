75c4edd8e1c11bac42be853497725a3b39617134
chelle tuerk <michelletuerk@gmail.com>
2017-03-22 21:11:05 -0600

    4 GET 4 POST 3 DELETE

--- server.js
+++ server.js
@@ -30,6 +30,26 @@ app.get('/api/v1/users', (request, response) => {
   })
 })
 
+app.get('/api/v1/comments', (request, response) => {
+  database('comments').select()
+  .then((comments) => {
+    response.status(200).json(comments)
+  })
+  .catch((error) => {
+    response.status(404).json({success: 'false'})
+  })
+})
+
+app.get('/api/v1/favorites', (request, response) => {
+  database('favorites').select()
+  .then((favorites) => {
+    response.status(200).json(favorites)
+  })
+  .catch((error) => {
+    response.status(404).json({success: 'false'})
+  })
+})
+
 app.post('/api/v1/users', (request, response) => {
   const { email, password } = request.body
   const user = { email, password }
@@ -42,16 +62,6 @@ app.post('/api/v1/users', (request, response) => {
       .catch(function(error) {
         response.status(422).json({success: 'false'})
       });
-  })
-})
-
-app.get('/api/v1/comments', (request, response) => {
-  database('comments').select()
-  .then((comments) => {
-    response.status(200).json(comments)
-  })
-  .catch((error) => {
-    response.status(404).json({success: 'false'})
   })
 })
 
@@ -70,30 +80,6 @@ app.post('/api/v1/comments', (request, response) => {
   })
 })
 
-app.delete('/api/v1/comments/:id', (request, response)=> {
-  const { id } = request.params
-  database('comments').where('id', id).select()
-    .then((comment)=> {
-      database('comments').where('id', id).select().del()
-      .then(function(comments) {
-        response.status(201).json({success: 'true'})
-      })
-      .catch(function(error) {
-        response.status(422).json({success: 'false'})
-      })
-    })
-})
-
-app.get('/api/v1/favorites', (request, response) => {
-  database('favorites').select()
-  .then((favorites) => {
-    response.status(200).json(favorites)
-  })
-  .catch((error) => {
-    response.status(404).json({success: 'false'})
-  })
-})
-
 app.post('/api/v1/favorites', (request, response) => {
   const { rating, songKickVenueId, userId } = request.body
   const comment = { rating, songKickVenueId, userId }
@@ -106,20 +92,6 @@ app.post('/api/v1/favorites', (request, response) => {
       .catch(function(error) {
         response.status(422).json({success: 'false'})
       });
-  })
-})
-
-app.delete('/api/v1/favorites/:id', (request, response)=> {
-  const { id } = request.params
-  database('favorites').where('id', id).select()
-    .then((comment)=> {
-      database('favorites').where('id', id).select().del()
-      .then(function(favorites) {
-        response.status(201).json({success: 'true'})
-      })
-      .catch(function(error) {
-        response.status(422).json({success: 'false'})
-      })
   })
 })
 
@@ -145,6 +117,101 @@ app.post('/api/v1/comments/:userId/:venueId', (request, response) => {
     });
   })
 })
+
+app.delete('/api/v1/users/:id', (request, response)=> {
+  const { id } = request.params
+  database('users').where('id', id).select()
+    .then((comment)=> {
+      database('users').where('id', id).select().del()
+      .then(function(favorites) {
+        response.status(201).json({success: 'true'})
+      })
+      .catch(function(error) {
+        response.status(422).json({success: 'false'})
+      })
+  })
+})
+
+app.delete('/api/v1/comments/:id', (request, response)=> {
+  const { id } = request.params
+  database('comments').where('id', id).select()
+    .then((comment)=> {
+      database('comments').where('id', id).select().del()
+      .then(function(comments) {
+        response.status(201).json({success: 'true'})
+      })
+      .catch(function(error) {
+        response.status(422).json({success: 'false'})
+      })
+    })
+})
+
+app.delete('/api/v1/favorites/:id', (request, response)=> {
+  const { id } = request.params
+  database('favorites').where('id', id).select()
+    .then((comment)=> {
+      database('favorites').where('id', id).select().del()
+      .then(function(favorites) {
+        response.status(201).json({success: 'true'})
+      })
+      .catch(function(error) {
+        response.status(422).json({success: 'false'})
+      })
+  })
+})
+
+// app.patch('/api/users/:id', (request, response)=> {
+//   const { id } = request.params
+//   database('users').where('id', id).select()
+//     .then((user)=> {
+//       let email = request.body.email
+//       database('users').where('id', id).select().update({ email })
+//         .then((users)=> {
+//           response.status(200).json(users)
+//         })
+//     })
+//     .catch((error)=> {
+//       console.log(error)
+//     })
+// })
+
+// app.patch('/api/comments/:id', (request, response)=> {
+//   const { id } = request.params
+//   const { body } = request.body
+//   database('comments').where('id', id).select()
+//     .then((comment)=> {
+//       database('comments').where('id', id).select().update({ body })
+//       .then(function(comments) {
+//         response.status(201).json({success: 'true'})
+//       })
+//       .catch(function(error) {
+//         response.status(422).json({success: 'false'})
+//       })
+//   })
+// })
+
+// app.patch('/api/comments/:id', (request, response)=> {
+//   const { id } = request.params
+//   database('comments').where('id', id).select()
+//     .then((comments)=> {
+//       const body = request.body.body
+//       database('comments').where('id', id).select().update({ body })
+//         .then((comments)=> {
+//           response.status(200).json(comments)
+//         })
+//     })
+//     .catch((error)=> {
+//       console.log(error)
+//     })
+// })
+
+
+
+
+
+
+
+
 
 app.listen(app.get('port'), () => {
   console.log(`${app.locals.title} is running on ${app.get('port')}.`)
